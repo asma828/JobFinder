3066a0616ab494ba094631984990697dd5f51b22
asma828 <asmalachhab66@gmail.com>
2026-02-17 12:47:53 +0000

    create the app routing system

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,45 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./components/auth/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./components/auth/register/register.component').then(m => m.RegisterComponent)
+  },
+  {
+    path: 'jobs',
+    loadComponent: () => import('./components/jobs/job-list/job-list.component').then(m => m.JobListComponent)
+  },
+  {
+    path: 'favorites',
+    loadComponent: () => import('./components/favorites/favorites.component').then(m => m.FavoritesComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'applications',
+    loadComponent: () => import('./components/applications/applications.component').then(m => m.ApplicationsComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./components/profile/profile.component').then(m => m.ProfileComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
